2e53b186065ac9d2ddcd1fec6516b1eed94e6b6c
nicole <eunjungnam@gmail.com>
2020-02-21 12:22:49 -0500

    style: added shop stuff text

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -1,10 +1,14 @@
 <?php get_header(); ?>
 <img id="tent" src="wp-content/themes/inhabitent/assets/images/logos/inhabitent-logo-tent-white.svg">
 <div class="onHero">
-
 <img id="logo" src="wp-content/themes/inhabitent/assets/images/logos/inhabitent-logo-full.svg">
 <?php the_post_thumbnail(); ?>
+</div>
 
+<!-- shopt stuff here -->
+<article>
+<h2 id="shop">SHOP STUFF</h2>
+</article>
 
 <?php if( have_posts() ) :
 
@@ -13,9 +17,8 @@
         the_post(); ?>
 
 
-</div>
-    <h2><?php the_title(); ?></h2>
-    <h3><?php the_permalink();?></h3>
+    <!-- <h2><?php the_title(); ?></h2> -->
+    <!-- <h3><?php the_permalink();?></h3> -->
     <?php the_content(); ?>
     
     <!-- Loop ends -->
