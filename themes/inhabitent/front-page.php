6f8a97fc1fc7e5189157aa05968f99b5fffbd4fa
nicole <eunjungnam@gmail.com>
2020-02-21 12:57:01 -0500

    chore: add initial grid

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -8,8 +8,47 @@
 <!-- shopt stuff here -->
 <article>
 <h2 id="shop">SHOP STUFF</h2>
-</article>
 
+<div class="shopGrid">
+  <div class="items">
+    <img src="wp-content/themes/inhabitent/assets/images/product-type-icons/do.svg">
+    <p>
+    Get back to nature with all </br>
+    the tools and toys you need to </br>
+    enjoy the great outdoors. </br>
+    </p>
+    <button class="shopBtn">DO STUFF</button>
+  </div>
+  <div class="items">
+    <img src="wp-content/themes/inhabitent/assets/images/product-type-icons/eat.svg">
+    <p>
+    Nothing beats food cooked</br>
+    over a fire. We have all you</br>
+    need for good camping eats. </br>
+    </p>
+    <button class="shopBtn">EAT STUFF</button>
+  </div>
+  <div class="items">
+    <img src="wp-content/themes/inhabitent/assets/images/product-type-icons/sleep.svg">
+    <p>
+    Get a good night's rest in the</br>
+    wild in a home away from</br>
+    home that travels well.</br>
+    </p>
+    <button class="shopBtn">SLEEP</button>
+  </div>
+  <div class="items">
+    <img src="wp-content/themes/inhabitent/assets/images/product-type-icons/wear.svg">
+    <p>
+    From flannel shirt to toques,</br>
+    look the part while roughing</br>
+    it in the great outdoors.</br>
+    </p>
+    <button class="shopBtn">WEAR STUFF</button>
+  </div>
+
+</div>
+</article>
 <?php if( have_posts() ) :
 
 //The WordPress Loop: loads post content 
