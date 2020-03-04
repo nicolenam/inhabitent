a68504156bd65a84b226f69cc0c33e909bea9afb
nicole <eunjungnam@gmail.com>
2020-03-04 14:31:13 -0500

    style: moved initial search bar to the right corner

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html <?php language_attributes();?>>
 <head>
-
     <?php wp_head();?>
     <meta charset="<?php bloginfo('charset');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  
 </head>
 <body <?php body_class();?>>
 <!-- <img id="homeBackground" src="wp-content/themes/inhabitent/assets/images/home-hero.jpg"> -->
@@ -19,7 +17,7 @@
         
 
             <?php wp_nav_menu(array(
-            'theme_location' => 'main'
+            'theme_location' => 'main',
             )) ;?>  
 
             <!-- SEARCH GOES HERE -->
